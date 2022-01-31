17245fbd90bcbabe8ca0d0a8360049a33bff3d66
jmp-home <contact@jmpalazzolo.io>
2022-01-31 20:02:31 +0100

    close #3 - Basic Express init

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('', (req, res) => {
+    res.status(200).send({
+        message: 'Hello world!'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
